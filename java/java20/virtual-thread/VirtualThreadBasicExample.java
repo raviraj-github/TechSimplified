5668204b6f0851831b3527758b0a3020d8e670e7
Ravi Raj <ravishankarraj90@gmail.com>
2023-07-18 02:46:44 +0530

    Create VirtualThreadBasicExample.java
    Added code for virtual thread basic example,

---
+++ java/java20/virtual-thread/VirtualThreadBasicExample.java
@@ -0,0 +1,55 @@
+/**
+ * VirtualThreadBasicExample
+ * This is an example Java class for demonstrating how to use virtual thread in java.
+ * <p>
+ * Copyright 2023 Ravi Raj
+ * <p>
+ * MIT License
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+ * is furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+ * FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
+
+public class VirtualThreadBasicExample {
+    public static void main(String[] args) {
+
+        //First way to create virtual thread.
+        Thread.startVirtualThread(() -> {
+            System.out.println("First Way: Hello World By Virtual Thread!");
+        });
+
+        //Another way to create the virtual thread with runnable. Here we are passing the task but not starting the thread.
+        Thread vt = Thread.ofVirtual().unstarted(() -> {
+            System.out.println("Another Way: Hello World By Virtual Thread!");
+        });
+
+        //We can start the thread whenever needed
+        vt.start();
+
+        //We can also check if a thread is virtual.
+        if (vt.isVirtual()) {
+            System.out.println("Another Way: I am Virtual Thread!");
+        }
+
+        /*This is essential for ensuring proper scheduling. Otherwise, there could be a situation where the
+        platform thread finishes early, while the virtual thread still awaits scheduling, as it relies on a
+        platform thread to execute.
+         */
+        LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(10));
+    }
+}
